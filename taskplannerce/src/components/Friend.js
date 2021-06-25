2dc3aa2c00db07b32a474162752f0cd227330326
Olman Castro <olcastro@estudiantec.cr>
2021-06-25 00:14:22 -0600

    [add] CSS Styles Student_Friends

---
+++ taskplannerce/src/components/Friend.js
@@ -0,0 +1,41 @@
+// Style
+import "../assets/styles/Friend.css";
+
+// React
+import React from "react";
+
+function Friend({name, email, time}) {
+  function accept_friend() {
+    // DB
+    console.log("Acept");
+  }
+
+  function delete_friend() {
+    // DB
+    console.log("Delete");
+  }
+  return (
+    <div className="friend_main">
+        <section className="img_main">
+            <h1>Img</h1>
+        </section>
+        <section className="info_main">
+            <div className="info">
+                <div>
+                    <h2>{name}</h2>
+                    <h3>{email}</h3>
+                </div>
+                <div>
+                    <h3>{time}</h3>
+                </div>
+            </div>
+            <div className="button_main">
+                <a onClick={accept_friend}>Accept</a>
+                <a onClick={delete_friend}>Delete</a>
+            </div>
+        </section>
+    </div>
+  );
+}
+
+export default Friend;
--- taskplannerce/src/containers/Student_Friends.js
+++ taskplannerce/src/containers/Student_Friends.js
@@ -1,11 +1,68 @@
 // Styles
+import "../assets/styles/Student_Friends.css";
 
 // React
 import React from "react";
+import Admin from "../components/Admin";
+import Friend from "../components/Friend";
 
 class Student_Friends extends React.Component {
+  admin_lists = [
+    {
+      name: "Andres Murillo",
+    },
+    {
+      name: "Juan de Dios",
+    },
+  ];
+
+  students_friends  = [
+    {
+      name: 'Randall Vinicio',
+      email: 'RV@estudiantec',
+      time: '3d'
+    },
+    {
+      name: 'Randall Fabricio',
+      email: 'RF@estudiantec',
+      time: '6d'
+    },
+    {
+      name: 'Randall Rolando',
+      email: 'RR@estudiantec',
+      time: '8d'
+    }
+  ]
+
   render() {
-    return <h1> Friends </h1>;
+    return (
+      <main> 
+        <section className="student_friends">
+          <h1>Friends</h1>
+          <div>
+            <input></input>
+            <button>New Request</button>
+          </div>
+          <div className="admin_table">
+          <ul>
+            {this.admin_lists.map((object) => (
+              <Admin key={object.name} {...object} />
+            ))}
+          </ul>
+        </div>
+        </section>
+        <section className="student_notifications">
+          <div>
+            <h2>Friend Request</h2>
+            <ul className="student_n_list">
+              {this.students_friends.map((object) => (
+                <Friend key={object.name} {...object} />
+              ))}
+            </ul>
+          </div>
+        </section>
+      </main>
+    );
   }
 }
 
