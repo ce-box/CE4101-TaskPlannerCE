e1337f6bb0876bacc89c5c5d9b740af061c108f1
hack <mendezamuel@gmail.com>
2021-06-24 23:21:16 -0600

    New Task

--- taskplannerce/src/components/Student_Board_Colums.js
+++ taskplannerce/src/components/Student_Board_Colums.js
@@ -6,15 +6,85 @@ import React from "react";
 // eslint-disable-next-line
 import { BrowserRouter as Router, NavLink } from "react-router-dom";
 
+function new_task() {
+  document.getElementsByClassName("task-new-popup")[0].style.display = "block";
+}
+function create() {
+  document.getElementsByClassName("task-new-popup")[0].style.display = "none";
+}
+function closeForm() {
+  document.getElementsByClassName("task-new-popup")[0].style.display = "none";
+}
+
 function Student_Board_Columns({ name }) {
-    var list_task = [{ name: "hola" }, { name: "conversacion" }, { name: "Adios" }];
+  var list_task = [
+    { name: "hola" },
+    { name: "conversacion" },
+    { name: "Adios" },
+  ];
 
   return (
     <div className="board_student_columns">
-        {name}
-        {list_task.map((object) => (
-            <Student_Board_Task key={object.name} {...object} />
-        ))}
+      <div className="task-new-popup">
+        <form className="form-container">
+          <h1>New Task</h1>
+          <div className="left_new_task">
+            <input type="text" placeholder="Task Title" />
+            <br />
+            <label for="description">Description </label>
+            <br />
+            <input
+              type="text"
+              placeholder="Description"
+              name="description"
+              required
+            />
+          </div>
+          <div className="right_new_task">
+            <label for="description">Assignee </label>
+            <br />
+            <select className="canton_student">
+                <option disabled selected>
+                  No one
+                </option>
+              </select>
+            <br />
+            <hr className="separator" />
+            <label for="description">TasK Priority </label>
+            <br />
+            <select className="canton_student">
+                <option disabled selected>
+                  No one
+                </option>
+              </select>
+            <br />
+            <hr className="separator" />
+            <label for="description">Start </label>
+            <br />
+            <input type="date" id="Start" name="Start"></input>
+            <br />
+            <label for="description">Due </label>
+            <br />
+            <input type="date" id="Due" name="Due"></input>
+            <br />
+            <hr className="separator" />
+            <label for="description">Dependencies </label>
+          </div>
+          <button type="button" className="btn" onClick={create}>
+            Create Board
+          </button>
+          <button type="button" className="btn close" onClick={closeForm}>
+            Close
+          </button>
+        </form>
+      </div>
+      {name}
+      <a className="button_new" onClick={new_task}>
+        +
+      </a>
+      {list_task.map((object) => (
+        <Student_Board_Task key={object.name} {...object} />
+      ))}
     </div>
   );
 }
