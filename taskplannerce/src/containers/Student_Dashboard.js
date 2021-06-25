e4a2f8b652ff159fe6a97014f9a1e39b59477aa8
Olman Castro <olcastro@estudiantec.cr>
2021-06-24 21:07:24 -0600

    [add] Student Dashboard

--- taskplannerce/src/containers/Student_Dashboard.js
+++ taskplannerce/src/containers/Student_Dashboard.js
@@ -3,69 +3,92 @@ import "../assets/styles/Student_Dashboard.css";
 
 // React
 import React from "react";
-import { Bar } from 'react-chartjs-2';
+import { Bar, Doughnut } from "react-chartjs-2";
 import Student_Board_Item from "../components/Student_Boart_Item";
 
 class Student_Dashboard extends React.Component {
-    dashboard_list = [
-        {
-          shared: 20,
-        },
-      ];
-      list_boards = [
-        { name: "Help" },
-        { name: "Datos" },
-        { name: "Lenguaje" },
-        { name: "Mate" },
-      ];
-      render() {
-        return (
-          <div className="dashboard_student">
-            <section>
-            <h1> Dashboard </h1>
-            Recently shared boards
-            <div className="professor_dash_board">
-              {this.list_boards.map((object) => (
-                <Student_Board_Item key={object.name} {...object} />
-              ))}
-            </div>
-            </section>
-            <section className="student_graf">
-              <div className="student_time_line">
-                <h2>Timeline of shared boards</h2>
-                <div>
-                <Bar
-                  data = {{
-                    labels: ['Jan', 'Feb', 'Social'],
-                    datasets: [
-                      {
-                        data: [1, 7, 8],
-                        backgroundColor: [
-                          'rgba(34, 108, 64, 1)',
-                          'rgba(87, 182, 97, 1)',
-                          'rgba(108, 196, 108, 1)',
-                        ],
-                        borderWidth: 1
-                      },
-                    ],
-                  }}
-                  height = {300}
-                  width = {600}
-                  options = {{
+  list_boards = [
+    { month: "Jan", boards: 2 },
+    { month: "Feb", boards: 1 },
+    { month: "Mar", boards: 7 },
+    { month: "Apr", boards: 1 },
+    { month: "May", boards: 3 },
+    { month: "Jun", boards: 5 },
+  ];
+  render() {
+    return (
+      <div className="dashboard_student">
+        <section>
+          <h1> Dashboard </h1>
+          Recently shared boards
+          <div className="professor_dash_board">
+            {this.list_boards.map((object) => (
+              <Student_Board_Item key={object.name} {...object} />
+            ))}
+          </div>
+        </section>
+        <section className="student_graf">
+          <div className="student_time_line">
+            <h2>Timeline of shared boards</h2>
+            <div>
+              <Bar
+                data = {{
+                    datasets: [{
+                      label: 'Boards',
+                      data: this.list_boards,
+                      backgroundColor: 'rgba(34, 108, 64, 1)'
+                  }] 
+                }}
+                height={300}
+                width={600}
+                options={{
+
+                    parsing: {
+                        xAxisKey: 'month',
+                        yAxisKey: 'boards'
+                    },
                     maintainAspectRatio: false,
                     scales: {
-                      x: {
-                        beginAtZero: true
-                      },
-                    }
-                  }} 
-                />
-                </div>
-              </div>
-            </section>
+                    x: {
+                      beginAtZero: true,
+                    },
+                  },
+                }}
+              />
+            </div>
           </div>
-        );
-    }
+          <div>
+            <h2>Percentage of academic boards</h2>
+            <div>
+                <Doughnut
+                data = {{
+                    labels: this.list_boards.map(function(lista){
+                        return lista.month}),
+                    datasets: [{
+                        data: this.list_boards.map(function(lista){
+                            return lista.boards }),
+                        backgroundColor: [
+                            'rgba(34, 108, 64, 1)',
+                            'rgba(87, 182, 97, 1)',
+                            'rgba(108, 196, 108, 1)',
+                            'rgba(162, 217, 107, 1)',
+                            'rgba(206, 226, 144, 1)',
+                            'rgba(251, 251, 196, 1)'
+                        ],
+                    }]
+                }}
+                height={300}
+                width={300}
+                options={{
+                    maintainAspectRatio: false,
+                }}
+                />
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Student_Dashboard;
